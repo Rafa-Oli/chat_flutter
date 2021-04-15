7eb5f4d1d9af2dbbcb65c079ea0ccbc74de58fcb
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-04-15 20:10:14 -0300

    integrating authentication with firebase

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,6 +1,8 @@
 import 'package:chat/models/auth_data.dart';
 import 'package:chat/widgets/auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -8,11 +10,37 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  void _handleSubmit(AuthData authData) {}
+  final _auth = FirebaseAuth.instance;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  Future<void> _handleSubmit(AuthData authData) async {
+    try {
+      if (authData.isLogin) {
+        await _auth.signInWithEmailAndPassword(
+            email: authData.email.trim(), password: authData.password);
+      } else {
+        await _auth.createUserWithEmailAndPassword(
+            email: authData.email.trim(), password: authData.password);
+      }
+    } on PlatformException catch (err) {
+      final msg =
+          err.message ?? 'An error has occurred ! Check your credentials';
+
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text(msg),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+    } catch (err) {
+      print(err);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: Theme.of(context).primaryColor,
       body: AuthForm(_handleSubmit),
     );
